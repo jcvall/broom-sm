de029ecc77fce022981b95ea7b5ec936b4c1de83
John C Vallier <jcvall@icloud.com>
2025-07-13 00:43:20 -0400

    update init

--- src/broom_sm/__init__.py
+++ src/broom_sm/__init__.py
@@ -30,7 +30,7 @@ else:
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "broom_sm"
+    dist_name = "broom_func"
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
